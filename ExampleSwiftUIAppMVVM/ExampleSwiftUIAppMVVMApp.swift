4ce4d27a6acbc70f66571e862d476d48ba66e37f
Mehmet Karatas <mehmet@appncode.com>
2025-04-02 14:32:59 +0100

    Initial Commit

---
+++ ExampleSwiftUIAppMVVM/ExampleSwiftUIAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIAppMVVMApp.swift
+//  ExampleSwiftUIAppMVVM
+//
+//  Created by Alper Karatas on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleSwiftUIAppMVVMTests/ExampleSwiftUIAppMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIAppMVVMTests.swift
+//  ExampleSwiftUIAppMVVMTests
+//
+//  Created by Alper Karatas on 02/04/2025.
+//
+
+import Testing
+@testable import ExampleSwiftUIAppMVVM
+
+struct ExampleSwiftUIAppMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
